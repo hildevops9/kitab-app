0c794b5f5bbd453162de9467b9820fec35295e55
hildevops9 <muhammadhilmya.devops@gmail.com>
2026-04-30 16:54:43 +0700

    fixing home

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../lib/api'
-import { useAuth } from '../contexts/Authcontext'
+import { useAuth } from '../contexts/AuthContext'
 
 const typeConfig = {
   QURAN:   { icon: '🕌', badge: 'Al-Quran',  badgeColor: '#1C3D2E' },
