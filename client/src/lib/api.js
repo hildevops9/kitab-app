bb616b8d66a5c90e304a35678d28b783a6190349
hildevops9 <muhammadhilmya.devops@gmail.com>
2026-04-30 16:43:01 +0700

    fixing iphone

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  // baseURL-nya dibikin relative gini aja biar mancing vercel.json beraksi
+  baseURL: '/api', 
   withCredentials: true,
 })
 
